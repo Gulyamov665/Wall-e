233dbe0b7cb0ebabcbe0007e831c865010a35985
Gulyamov665 <gulyamov640@gmail.com>
2024-05-05 17:55:05 +0500

    settings change

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,30 @@ SECRET_KEY = "django-insecure-6x%gf7=+s+2+*h!$vrs=^-qudg#mb_u80p669lx*$77ur@y5fb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+
+CORS_ALLOW_HEADERS = [
+    "access-control-allow-origin",
+    "authorization",
+    "content-type",
+]
+
 
 PASSWORD_MIN_LENGHT = 8
 
@@ -52,11 +75,13 @@ INSTALLED_APPS = [
     "users",
     "tasks",
     "core",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
