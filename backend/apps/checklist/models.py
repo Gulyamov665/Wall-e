d7634e86458770a4ac8dbeac171c5c547e027f70
Gulyamov665 <gulyamov640@gmail.com>
2024-07-14 18:23:40 +0500

    added checklist module v2

--- backend/apps/checklist/models.py
+++ backend/apps/checklist/models.py
@@ -2,26 +2,48 @@ from django.db import models
 from tasks.utils.directory_path import upload_path_product_images
 from core.models import BaseModel
 
+
 class Brand(BaseModel):
     name = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.name}'
+        return f"{self.name}"
 
 
 class Product(BaseModel):
     name = models.CharField(max_length=255, null=True, blank=True)
-    brand = models.ForeignKey("checklist.Brand", on_delete=models.CASCADE, null=True, blank=True)
-    image = models.FileField(upload_to=upload_path_product_images, null=True, blank=True)
+    brand = models.ForeignKey(
+        "checklist.Brand", on_delete=models.CASCADE, null=True, blank=True
+    )
+    image = models.FileField(
+        upload_to=upload_path_product_images, null=True, blank=True
+    )
 
     def __str__(self) -> str:
         return self.name
-    
 
 
 class Checklist(BaseModel):
-    product = models.ForeignKey("checklist.Product", on_delete=models.CASCADE, null=True, blank=True)
-    task_id = models.ForeignKey("tasks.Task", on_delete=models.CASCADE, null=True, blank=True)
-    task_comment = models.ForeignKey("tasks.TaskComments", on_delete=models.CASCADE, null=True, blank=True)
+    product = models.ForeignKey(
+        "checklist.Product",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="product",
+    )
+    task_id = models.ForeignKey(
+        "tasks.Task",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="task_id",
+    )
+    task_comment = models.ForeignKey(
+        "tasks.TaskComments",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="task_comment",
+    )
     is_checked = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True, editable=False)
--- backend/apps/tasks/models.py
+++ backend/apps/tasks/models.py
@@ -114,7 +114,7 @@ class TaskComments(BaseModel):
         related_name="task_comments",
     )
     comment = models.TextField(null=True, blank=True)
-    checklist = models.ManyToManyField("checklist.Product", through="checklist.Checklist")
+    checklist = models.ManyToManyField("checklist.Product", through="checklist.Checklist", related_name='checklist')
 
     # def __str__(self):
     # return f"{self.task.name} | {self.comment}"
